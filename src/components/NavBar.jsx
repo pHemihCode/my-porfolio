cf32b78341e2f757a92ab4cc7d91e9910305f4df
Femi Akintan <akintanfemi100@gmail.com>
2023-08-03 13:04:38 +0100

    Updated files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,13 +15,13 @@ function NavBar() {
     } 
   return (
      <div>
-       <div className="navbar h-20 w-screen fixed top-0 md:flex md:flex-row md:items-center md:justify-between md:pl-5 md:pr-10 md:z-10 lg:pl-10 lg:pr-16 xl:pr-24">
+       <div className="navbar z-10 h-20 w-screen fixed top-0 md:flex md:flex-row md:items-center md:justify-between md:pl-5 md:pr-10 md:z-10 lg:pl-10 lg:pr-16 xl:pr-24">
        <div className="logo w-32 h-32 md:h-fit">
            <Link to='/'>
                 <img src={Logo} alt='pHoenix' />
            </Link>
        </div>
-        <nav onClick={handleClick} className={hamburger ? 'hidden md:flex md:space-x-6 md:activelink xl:space-x-10 xl:text-lg' : 'show bg-blue-400'}>
+        <nav onClick={handleClick} className={hamburger ? 'hidden md:flex md:space-x-6 md:activelink xl:space-x-10 xl:text-lg' : 'show bg-blue-400 z-10'}>
             <NavLink to='/' > <AiOutlineHome className='i md:hidden md:active'/>Home</NavLink>
             <NavLink to='/about'><BsPersonCheck className='i md:hidden'/>About</NavLink>
             <NavLink to='/projects'><AiOutlineProject className='i md:hidden'/>Projects</NavLink>
